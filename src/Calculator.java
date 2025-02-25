73cdecf5b5275eb0e2148eeec9db763f19a6e40d
Sachin Kadam <sachin.kadam@calsoftinc.com>
2025-02-25 14:17:31 +0530

    Create Calculator.java

---
+++ src/Calculator.java
@@ -0,0 +1,25 @@
+public class Calculator {
+
+    // Method to add two numbers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method to subtract two numbers
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Method to multiply two numbers
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Method to divide two numbers
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return (double) a / b;
+    }
+}
